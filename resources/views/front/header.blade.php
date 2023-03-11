fb1485d84d68c279dfb2b6abac0eb65db041d592
Karan Gurnani <karangurnani@Karans-MacBook-Pro.local>
2023-03-12 00:56:54 +0530

    added logo for front page

--- resources/views/front/header.blade.php
+++ resources/views/front/header.blade.php
@@ -2,8 +2,11 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ App\Models\Customization::get()->first()->title ?? 'Greetings' }}</title>
+    <link rel="icon" type="image/x-icon" href="{{asset('storage/favicon/'.App\Models\Customization::get()->first()->favicon) }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link href="{{asset('css/style2.css')}}" rel="stylesheet">
+
     <script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
     @if(!empty($customization->header_script))
